39bc95a5415b909ceb666a6e86d6620ce73660d8
github_cloudlabsuser_2135@cloudlabsaiuser.com <github_cloudlabsuser_2135@cloudlabsaiuser.com>
2024-06-18 07:53:36 +0000

    Add NUnit unit tests for UserController CRUD operations

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,9 @@
 using CRUD_application_2.Models;
 using System.Linq;
 using System.Web.Mvc;
- 
+
+// ou will be creating an application named Contact Database Application, a C# application, with the help of Github Copilot, which will let the users save the contact details of people as per requirements and also carry out the basic functions like editing their details, deleting their profiles, and so on. You will be provided with the skeleton of the application already, but you will need to build the functionalities inside the UserController.cs file by yourself. Your task is to complete these methods by utilizing GitHub Copilot to generate the necessary code, guided by the comments provided in the file.
+
 namespace CRUD_application_2.Controllers
 {
     public class UserController : Controller
@@ -10,57 +12,82 @@ namespace CRUD_application_2.Controllers
         // GET: User
         public ActionResult Index()
         {
-            // Implement the Index method here
+            return View(userlist);
         }
  
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
-            // Implement the details method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
  
         // GET: User/Create
         public ActionResult Create()
         {
-            //Implement the Create method here
+            return View();
         }
  
         // POST: User/Create
         [HttpPost]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
+            userlist.Add(user);
+            return RedirectToAction("Index");
         }
  
         // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
-            // This method is responsible for displaying the view to edit an existing user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
  
         // POST: User/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, User user)
         {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+            if (existingUser == null)
+            {
+                return HttpNotFound();
+            }
+            // Update properties here
+            existingUser.Name = user.Name;
+            existingUser.Email = user.Email;
+            // Add other properties as needed
+            return RedirectToAction("Index");
         }
  
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
-            // Implement the Delete method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
  
         // POST: User/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
-            // Implement the Delete method (POST) here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user != null)
+            {
+                userlist.Remove(user);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
